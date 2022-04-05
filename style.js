4ebaf6d1c3d3f8d92f5f9f8f2fff3a94bfb4c0f0
Adam McLane <mclanea92@gmail.com>
2022-04-05 14:53:02 -0400

    added color to times

--- style.js
+++ style.js
@@ -4,6 +4,43 @@ var headerDate = moment().format('dddd, MMMM Do');
 
 
 
-
+// Adds the day and date to the top of page
 $('#currentDay').text(headerDate);
 
+
+
+
+//Change textarea background color based on time
+var checkTime = function () {
+
+    //Get Current time
+    var currentTime = moment().format('H');
+
+    //get all elements with class "timeColor"
+    var timeBlockElements = $(".timeColor");
+
+    //loop through taskarea classes
+    for (var i = 0 ; i < timeBlockElements.length ; i++) {
+
+        //Get element i's ID as a string
+        var elementID = timeBlockElements[i].id;
+
+        //get element by ID
+        var manipID = document.getElementById(timeBlockElements[i].id)
+
+        //remove any old classes from element
+        $(timeBlockElements[i].id).removeClass(".present .past .future");
+
+        // apply new class if task is present/past/future
+        if (elementID < currentTime) {
+            $(manipID).addClass("past");
+        } else if (elementID > currentTime) {
+            $(manipID).addClass("future");
+        } else {
+            $(manipID).addClass("present");
+        }
+    }
+}
+
+// checkTime every 5 minutes
+setInterval(checkTime(), (1000 * 60) * 5);
